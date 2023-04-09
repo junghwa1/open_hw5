4b8e549aeb40f416255ac4c4a0cdbdf077468387
duawn <duawhk0508@naver.com>
2023-04-09 17:02:41 +0900

    과제

---
+++ Web_browser6_2/app/src/main/java/com/example/web_browser6_2/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.web_browser6_2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.web_browser6_2.R;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.web);
+
+        edtUrl = findViewById(R.id.edtUrl);
+        btnGo = findViewById(R.id.btnGo);
+        btnBack = findViewById(R.id.btnBack);
+        web = findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.loadUrl(edtUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.goBack();
+            }
+        });
+
+    }
+
+    class CookWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+}
